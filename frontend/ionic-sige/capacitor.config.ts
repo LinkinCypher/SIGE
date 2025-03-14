3212370612dfb225c9613964858f8f19adc82d4f
LinkinCypher <linkincypher@hotmail.com>
2025-03-14 11:36:24 -0500

    Instalacion Ionic

---
+++ frontend/ionic-sige/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-sige',
+  webDir: 'www'
+};
+
+export default config;

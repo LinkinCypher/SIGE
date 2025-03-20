c51327db0187b49726416078ae2c41bd71d9f986
LinkinCypher <linkincypher@hotmail.com>
2025-03-20 08:48:07 -0500

    app-inventario inicio y configuración

--- backend/app-inventario/src/app.module.ts
+++ backend/app-inventario/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    //Configuración de variables de entorno
+    ConfigModule.forRoot({
+      isGlobal:true,
+    }),
+
+    // Conexión a MongoDB
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (ConfigService: ConfigService) => ({
+        uri: ConfigService.get<string>('MONGODB_URI'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
